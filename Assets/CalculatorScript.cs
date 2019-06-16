013578642e47968cdb530fa4a7a7e487f9c8993a
JasonXieyf <JasonXieyf@hotmail.com>
2019-06-15 19:23:17 -0700

    Initial commit

---
+++ Assets/CalculatorScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CalculatorScript : MonoBehaviour
+{
+    [SerializeField] InputField input1;
+    [SerializeField] InputField input2;
+    [SerializeField] Text output;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Add ()
+    {
+        int num1 = int.Parse(input1.text);
+        int num2 = int.Parse(input2.text);
+        int result = num1 + num2;
+        output.text = result.ToString();
+    }
+
+    public void Subtract()
+    {
+        int num1 = int.Parse(input1.text);
+        int num2 = int.Parse(input2.text);
+        int result = num1 - num2;
+        output.text = result.ToString();
+    }
+
+    public void Multiply()
+    {
+        int num1 = int.Parse(input1.text);
+        int num2 = int.Parse(input2.text);
+        int result = num1 * num2;
+        output.text = result.ToString();
+    }
+
+    public void Divide()
+    {
+        int num1 = int.Parse(input1.text);
+        int num2 = int.Parse(input2.text);
+        int result = num1 / num2;
+        output.text = result.ToString();
+    }
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene("SimpleCalculator2");
+    }
+}
---
+++ Assets/CalculatorScript2.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CalculatorScript2 : MonoBehaviour
+{
+    enum Operation { Void, Add, Subtract, Multiply, Divide };
+
+    [SerializeField]Text displayText;
+
+    int lastNum;
+    Operation currOperation;
+    bool newNum;
+
+	void Start ()
+    {
+        PressReset();
+	}
+
+    public void PressNum(int num)
+    {
+        int currNum = int.Parse(displayText.text);
+        if(newNum)
+        {
+            lastNum = currNum;
+            currNum = 0;
+            newNum = false;
+        }
+        if(currNum == 0)
+        {
+            currNum = num;
+        }
+        else
+        {
+            currNum = currNum * 10 + num;
+        }
+        displayText.text = currNum.ToString();
+    }
+
+    public void PressReset()
+    {
+        displayText.text = "0";
+        lastNum = 0;
+        currOperation = Operation.Void;
+        newNum = false;
+    }
+
+    public void PressAdd()
+    {
+        if(currOperation != Operation.Void && !newNum)
+        {
+            Calculate();
+        }
+        currOperation = Operation.Add;
+        newNum = true;
+    }
+
+    public void PressSubtract()
+    {
+        if (currOperation != Operation.Void && !newNum)
+        {
+            Calculate();
+        }
+        currOperation = Operation.Subtract;
+        newNum = true;
+    }
+
+    public void PressMultiply()
+    {
+        if (currOperation != Operation.Void && !newNum)
+        {
+            Calculate();
+        }
+        currOperation = Operation.Multiply;
+        newNum = true;
+    }
+
+    public void PressDivide()
+    {
+        if (currOperation != Operation.Void && !newNum)
+        {
+            Calculate();
+        }
+        currOperation = Operation.Divide;
+        newNum = true;
+    }
+
+    private void Calculate()
+    {
+        int result = 0;
+        int currNum = int.Parse(displayText.text);
+        switch (currOperation)
+        {
+            case Operation.Void:
+                result = currNum;
+                break;
+            case Operation.Add:
+                result = lastNum + currNum;
+                break;
+            case Operation.Subtract:
+                result = lastNum - currNum;
+                break;
+            case Operation.Multiply:
+                result = lastNum * currNum;
+                break;
+            case Operation.Divide:
+                result = lastNum / currNum;
+                break;
+        }
+        currOperation = Operation.Void;
+        displayText.text = result.ToString();
+    }
+
+    public void PressResult()
+    {
+        if (currOperation != Operation.Void && !newNum)
+        {
+            Calculate();
+        }
+        currOperation = Operation.Void;
+        newNum = true;
+    }
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene("SimpleCalculator1");
+    }
+}
